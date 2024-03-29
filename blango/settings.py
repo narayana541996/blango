1f89ca80677b272918c457e0029a6f66bcd45c32
Lakshmi Narayana <narayana541996@gmail.com>
2024-03-29 12:20:16 +0000

    Finish security and passwords

--- blango/settings.py
+++ blango/settings.py
@@ -38,6 +38,13 @@ class Dev(Configuration):
     CSRF_COOKIE_SAMESITE = 'None'
     SESSION_COOKIE_SAMESITE = 'None'
 
+      PASSWORD_HASHERS = [
+      'django.contrib.auth.hashers.Argon2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+      'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+      'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+  ]
+
     # Application definition
 
     INSTALLED_APPS = [
